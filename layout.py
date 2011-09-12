bd9b434be38d156b52fc0da8c733d7275b043b76
Ubuntu <ubuntu@ip-10-130-57-144.ap-southeast-1.compute.internal>
2011-09-12 14:59:33 +0000

    implemented folding at space. 	modified:   layout.py

--- layout.py
+++ layout.py
@@ -31,6 +31,7 @@ def add(*xys):
 
 class DC(object):
   def __init__(self, size):
+    self.size = size
     self.img = Image.new('RGBA', size)
     self.draw = ImageDraw.Draw(self.img)
     self.boxes = []
@@ -47,15 +48,96 @@ class DC(object):
     self.img.save(buf, mime2fext(mimetype))
     return buf.getvalue()
 
-  def add(self, name, **arg):
-    print 'dc:add'
-    assert name == 'text'
+  def fold(self, box):
+    self.fold_CRLF(box)
+
+  def fold_CRLF(self, box):
+    print 'fold_CRLF "%s"'%(box.text,)
+    lines = box.text.splitlines()
+    for line in lines:
+      p = box.get_param()
+      b = self.make_box(p, line)
+      if self.is_X_overflaw(b):
+        self.fold_WHITE(b)
+      else:
+        self.push(b)
+
+  def fold_WHITE(self, box):
+    print 'fold_WHITE "%s"'%(box.text,)
+    last = 0
+    line = box.text
+    end = len(line)
+    p = box.get_param()
+    for n, c in enumerate(line):
+      if c not in ' \t\v\n' and n < end:
+        pass
+      else:
+        b = self.make_box(p, text=line[:n])
+        print 'trying %3d "%s"'%(n, b.text,)
+        if not self.is_X_overflaw(b):
+          last = n
+          continue
+        else:
+          if last == 0:
+            #no way to fold. give up
+            print 'no way to fold. give up'
+            self.push(box)
+            return
+          else:
+            break
+
+    b = self.make_box(p, line)
+    if not self.is_X_overflaw(b):
+      print 'no need to fold.'
+      self.push(b)
+      return 
+
+    b = self.make_box(p, line[:last])
+    self.push(b)
+    rest = line[last:] 
+    rest = rest.strip()
+    if rest:
+      print 'rest: "%s"'%(rest,)
+      box = self.make_box(p, rest)
+      self.fold_WHITE(box)
+
+
+  def push(self, box):
+    print 'push "%s"'%(box.text,)
+    rect = box.get_rect()
+    self.boxes.append(box)
+    self.current[1] = rect[1][1]
+    
+  def pop(self):
+    assert self.boxes
+    box = self.boxes.pop()
+    if self.boxes:
+      last = self.boxes[-1]
+      rect = last.get_rect()
+      self.current[1] = rect[1][1]
+    else:
+      self.current[1] = 0
+    return box
+    
+  def make_box(self, arg, text=None):
     arg.update({'topleft': tuple(self.current)})
-    self.boxes.append(Box(**arg))
-    last = self.boxes[-1]
-    print last.get_rect()
-    self.current[1] = last.get_rect()[1][1]
-    print self.current
+    if text:
+      arg.update({'text': text})
+    return Box(**arg)
+
+  def is_X_overflaw(self, box):
+    rect = box.get_rect()
+    return rect[1][0] > self.size[0]
+
+  def add(self, name, **arg):
+    print 'add"%s"'%(arg['text'],)
+    assert name == 'text'
+    box = self.make_box(arg)
+
+    if not self.is_X_overflaw(box):
+      self.push(box)
+    else:
+      self.fold(box)
 
   def render(self):
     for box in self.boxes:
@@ -67,6 +149,11 @@ class Model(object):
   def __init__(self, padding):
     assert len(padding) == 2
     self.padding = padding
+
+  def get_param(self):
+    d = {}
+    d.update(self.__dict__)
+    return d
 
   def render(self, dc):
     pass
